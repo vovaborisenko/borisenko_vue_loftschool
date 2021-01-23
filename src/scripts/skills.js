a0d478d5a24a178cad6f7b79fdd20b46c1b0d47e
Uladzimir Barysenka <vovaborisenko@live.com>
2021-01-23 13:32:59 +0300

    add skills vue widget

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,34 @@
-console.log('this is the skills module');
+import Vue from 'vue';
+
+const SkillsListItem = {
+  props: ['title', 'percent'],
+  template: '#skill-item',
+  methods: {
+    calcStrokeDashoffset() {
+      const strokeDasharray = parseInt(getComputedStyle(this.$refs['colored-circle']).strokeDasharray);
+
+      return strokeDasharray * (100 - this.percent)/100;
+    },
+  },
+  mounted() {
+    this.$refs['colored-circle'].style.strokeDashoffset = this.calcStrokeDashoffset() ;
+  }
+};
+
+const SkillsList = {
+  props: ['title', 'skills'],
+  template: '#skills-category',
+  components: { SkillsListItem },
+};
+
+new Vue({
+  el: '#skills-list',
+  template: '#skills-widget',
+  components: { SkillsList },
+  data: () => ({
+    skillsGroups: [],
+  }),
+  created() {
+    this.skillsGroups = require('./../data/skills.json');
+  }
+});
