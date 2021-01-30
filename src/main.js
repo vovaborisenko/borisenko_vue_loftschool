8830a80cbda26c69721013e507f32cbb9704647d
Uladzimir Barysenka <vovaborisenko@live.com>
2021-01-30 19:50:27 +0300

    add vue in projects block

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ if (process.env.NODE_ENV === "development") {
 
 import "./scripts/skills";
 import "./scripts/reviews";
-import "./scripts/menu"
+import "./scripts/projects";
+import "./scripts/menu";
