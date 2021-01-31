f091c56a32280d31324430125173bee956b9e53c
Uladzimir Barysenka <vovaborisenko@live.com>
2021-01-31 16:27:28 +0300

    add app-user vue component

---
+++ src/admin/components/app-user/app-user.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .app-user
+    avatar.app-user__avatar(
+      :size="size"
+      :src="src"
+    )
+    .app-user__name
+      slot
+</template>
+
+<script>
+import Avatar from "components/avatar";
+
+export default {
+  name: 'app-user',
+  components: { Avatar },
+  props: {
+    size: {
+      type: String,
+      default: '3',
+    },
+    src: {
+      type: String,
+    }
+  },
+}
+</script>
+
+<style lang="postcss" src="./app-user.pcss"></style>
